e52186d26f63162571ad63656d049377811c882c
Nirbhay Walia <nwalia087@gmail.com>
2024-06-12 13:03:16 +0530

    commit

--- src/components/newsContainer.js
+++ src/components/newsContainer.js
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./newsItem";
 import Loading from "./loading";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class NewsContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      pageSize: 11,
-      totalResults: 0,
-    };
-  }
+const NewsContainer = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(11);
+  const [totalResults, setTotalResults] = useState(0);
 
-  activeclass = () => {
+  const activeclass = () => {
     const dropDownMenu = document.getElementById("catagory-Selector");
     const dropDownItems = Array.from(dropDownMenu.getElementsByClassName("dropdown-item"));
     dropDownItems.forEach((element) => {
-      if (element.textContent === this.props.catagory) {
+      if (element.textContent === props.catagory) {
         element.classList.add("active");
       } else {
         element.classList.remove("active");
@@ -27,93 +22,84 @@ export class NewsContainer extends Component {
     });
   };
 
-  fetchMoreData = async () => {
-    const { page, pageSize } = this.state;
-    const { catagory } = this.props;
+  const fetchMoreData = async () => {
+    setLoading(true);
 
-    this.setState({ loading: true });
-
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${catagory}&apiKey=${this.props.ApiKey}&page=${page}&pageSize=${pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.catagory}&apiKey=${props.ApiKey}&page=${page}&pageSize=${pageSize}`;
     const data = await fetch(url);
     const parsedData = await data.json();
 
-    // 2. Efficient State Update
     const newArticles = parsedData.articles.filter(
-      (newArticle) => !this.state.articles.some((article) => article.url === newArticle.url)
+      (newArticle) => !articles.some((article) => article.url === newArticle.url)
     );
 
-    this.setState((prevState) => ({
-      articles: [...prevState.articles, ...newArticles],
-      totalResults: parsedData.totalResults,
-      loading: false,
-      page: prevState.page + 1,
-    }));
+    setArticles((prevArticles) => [...prevArticles, ...newArticles]);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    setPage((prevPage) => prevPage + 1);
   };
 
-  async updatePage() {
-    this.props.setProgress(10);
-    const { catagory } = this.props;
+  const updatePage = async () => {
+    props.setProgress(10);
+    setLoading(true);
 
-    this.setState({ loading: true });
-
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${catagory}&apiKey=${this.props.ApiKey}&page=1&pageSize=${this.state.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.catagory}&apiKey=${props.ApiKey}&page=1&pageSize=${pageSize}`;
     const data = await fetch(url);
-    this.props.setProgress(30);
+    props.setProgress(30);
     const parsedData = await data.json();
-    this.props.setProgress(50);
+    props.setProgress(50);
 
-    this.activeclass();
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-      page: 2,
-    });
-    this.props.setProgress(100);
-  }
+    activeclass();
 
-  async componentDidMount() {
-    this.updatePage();
-  }
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    setPage(2);
+    props.setProgress(100);
+  };
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.catagory !== this.props.catagory) {
-      this.setState({ page: 1, articles: [] }, this.updatePage);
+  useEffect(() => {
+    updatePage();
+  }, [props.catagory]);
+
+  const componentDidUpdate = async (prevProps) => {
+    if (prevProps.catagory !== props.catagory) {
+      setPage(1);
+      setArticles([]);
+      updatePage();
     }
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <h1 className="h1 text-center" style={{ textTransform: "capitalize" }}>
-          Burger Bulletin - {this.props.catagory} news
-        </h1>
-        {this.state.loading && <Loading />}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length < this.state.totalResults}
-          loader={<h4 className="text-center">Loading...</h4>}>
-          <div className="container justify-content-center justify-content-lg-evenly d-flex flex-wrap">
-            {this.state.articles
-              .filter((element) => element.title && element.description && element.url && element.urlToImage)
-              .map((element) => (
-                <NewsItem
-                  key={element.url}
-                  title={element.title}
-                  description={element.description}
-                  imgUrl={element.urlToImage}
-                  url={element.url}
-                  dayNDate={element.publishedAt}
-                  source={element.source.name}
-                  author={element.author}
-                />
-              ))}
-          </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1 className="h1 text-center" style={{ textTransform: "capitalize" }}>
+        Burger Bulletin - {props.catagory} news
+      </h1>
+      {loading && <Loading />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length < totalResults}
+        loader={<h4 className="text-center">Loading...</h4>}>
+        <div className="container justify-content-center justify-content-lg-evenly d-flex flex-wrap">
+          {articles
+            .filter((element) => element.title && element.description && element.url && element.urlToImage)
+            .map((element) => (
+              <NewsItem
+                key={element.url}
+                title={element.title}
+                description={element.description}
+                imgUrl={element.urlToImage}
+                url={element.url}
+                dayNDate={element.publishedAt}
+                source={element.source.name}
+                author={element.author}
+              />
+            ))}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
 
 export default NewsContainer;
